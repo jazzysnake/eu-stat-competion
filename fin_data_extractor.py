5e9f57adf9f6440dc77e80d9e1ae359f917d6b6c
jazzysnake <dotya77@gmail.com>
2025-05-19 12:30:19 +0200

    missing files added

---
+++ fin_data_extractor.py
@@ -0,0 +1,99 @@
+import logging
+import asyncio
+
+from google.genai import types
+
+import genai_utils
+import models
+import valkey_stores
+
+from utils import batched
+
+class FinDataExtractor:
+    def __init__(
+        self,
+        gen_client: genai_utils.GenaiClient,
+        conversation_store: valkey_stores.ConversationStore,
+        report_link_store: valkey_stores.AnnualReportLinkStore,
+        report_info_store: valkey_stores.AnnualReportInfoStore,
+        report_directory: str,
+        concurrent_threads: int = 1,
+    ) -> None:
+        self.gen_client = gen_client
+        self.report_link_store = report_link_store
+        self.conversation_store = conversation_store
+        self.report_directory = report_directory
+        self.report_info_store = report_info_store
+        self.concurrent_threads = concurrent_threads
+
+
+    async def run(self) -> None:
+        companies = self.report_link_store.get_companies()
+
+        for company_batch in batched(companies, self.concurrent_threads):
+            tasks = [self.process_company(c) for c in company_batch]
+            await asyncio.gather(*tasks)
+
+    async def process_company(self, company: str) -> None:
+        extracted_data = self.report_info_store.get(company)
+        if extracted_data is not None:
+            return
+        link = self.report_link_store.get(company)
+        if not isinstance(link, models.AnnualReportLinkWithPaths):
+            logging.warning(f'Skipping data extraction for {company}, as report is not available locally')
+            return
+        if link is None or link.local_path is None:
+            # keep the linter happy, previous condition makes this redundant
+            logging.warning(f'Local path is missing, fallback to GCS/download is not yet implemented, skipping company {company}')
+            return
+        try:
+            buffer = None
+            with open(link.local_path, 'rb') as f:
+                buffer = f.read()
+
+            mime = 'application/pdf' if link.local_path.endswith('.pdf') else 'text/html'
+            file = types.Part.from_bytes(data=buffer,mime_type=mime)
+            report = await self.extract_data_from_report(company,file)
+            self.report_info_store.store(company, report)
+
+        except Exception as e:
+            logging.error(f'Failed to extract data from report for company: {company}, cause {e}')
+
+        
+    async def extract_data_from_report(
+        self,
+        company: str,
+        report_file: types.Part,
+    ) -> models.AnnualReportInfo:
+        prompt = """Extract the relevant financial data from the attached annual report according to specified in the format.
+        
+        Notes:
+        - Make sure to extract assset values/net turnover in their most expanded integer form. (If the report specifies them in thousands or millions/billions etc, make sure to input the full value)
+        - Similarly for employee count, extract the expanded integer forms.
+        - Only extract information you explicitly found in the attached document, base your answer on facts.
+        - Avoid repeating marketing slop when summarizing the main activity. Look at the facts and collect the main industries and sectors the company participates in (if possible order them by priority).
+        """
+        msg = self.gen_client.get_simple_message(prompt)
+        self.conversation_store.store(
+            company,
+            'info_extract',
+            msg,
+        )
+        msg[0].parts.append(report_file)
+        res = await self.gen_client.generate(
+            msg,
+            thinking_budget=2048,
+            model=genai_utils.PRO,
+            response_schema=models.AnnualReportInfo,
+        )
+        if res.candidates is None:
+            raise genai_utils.GenerationError('Failed to generate response')
+        msg.append(res.candidates[0].content)
+        self.conversation_store.store(
+            company,
+            'info_extract',
+            msg,
+        )
+        return models.AnnualReportInfo.model_validate_json(res.text)
+
+
---
+++ report_downloader.py
@@ -0,0 +1,99 @@
+import os
+import httpx
+import logging
+import asyncio
+
+import valkey_stores
+
+from utils import batched
+from models import AnnualReportLink, AnnualReportLinkWithPaths
+from crawler import HTMLDownloader
+from pdf_downloader import PDFDownloader
+
+class DownloadError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class ReportDownloader:
+    def __init__(
+        self,
+        report_link_store: valkey_stores.AnnualReportLinkStore,
+        report_download_directory: str,
+        concurrent_threads: int = 1,
+    ) -> None:
+        self.report_link_store = report_link_store
+        self.report_download_directory = report_download_directory
+        self.concurrent_threads = concurrent_threads
+        self.pdf_downloader = PDFDownloader()
+        self.html_downloader = HTMLDownloader()
+
+    async def run(self) -> None:
+        companies = self.report_link_store.get_companies()
+        if len(companies) == 0:
+            return
+        
+        for company_batch in batched(companies, self.concurrent_threads):
+            tasks = [self.process_company(c) for c in company_batch]
+            await asyncio.gather(*tasks)
+
+    async def process_company(
+        self,
+        company: str,
+    ) -> None:
+        try:
+            reportlink = self.report_link_store.get(company)
+            if reportlink is None:
+                logging.error(f'Report link is missing for company {company}, skipping download')
+                return
+            if reportlink is not None:
+                if isinstance(
+                    reportlink,
+                    AnnualReportLinkWithPaths,
+                ) and reportlink.local_path is not None:
+                    return
+            logging.info(f'Downloading report for {company}...')
+            fname = await self.download_annual_report(reportlink, company)
+            self.report_link_store.add_local_path(company, fname)
+        except DownloadError as e:
+            logging.error(e, exc_info=True)
+        except Exception as e:
+            logging.error(f'Unexpected error occured: {e}', exc_info=True)
+
+
+    @staticmethod
+    def __clean_filename(name: str) -> str:
+        return name.replace(' ', '_').replace('\\', '').replace('/', '')
+        
+    async def download_annual_report(
+        self,
+        report_link: AnnualReportLink,
+        company: str,
+    ) -> str:
+        if report_link.link is None:
+            raise ValueError('Link must not be None')
+        refyear = '' if report_link.refyear is None else str(report_link.refyear)
+        fname = '_'.join([company,refyear])
+        try:
+            is_pdf = await self.pdf_downloader.is_pdf(report_link.link)
+            print('is_pdf', is_pdf)
+            fname = fname + '.pdf' if is_pdf else fname + ".html"
+            fname = ReportDownloader.__clean_filename(fname)
+            fname = os.path.join(self.report_download_directory, fname)
+            if is_pdf:
+                try:
+                    await self.pdf_downloader.download_async(report_link.link, fname)
+                except httpx.HTTPStatusError as e:
+                    if '403' in str(e):
+                        print(f'Retrying download with spoofed browser agent for {company}')
+                        await self.pdf_downloader.download_async(
+                            report_link.link,
+                            fname,
+                            spoof_browser_user_agent=True,
+                        )
+
+                return fname
+            await self.html_downloader.download(report_link.link, fname)
+            return fname
+        except Exception as e:
+            raise DownloadError(f'Failed to download report for company {company}') from e
+
